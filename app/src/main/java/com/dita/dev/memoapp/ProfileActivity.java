cb10e3862b9204e981ff492a43c5c53feadb68a2
Michael Bukachi <michaelbukachi@gmail.com>
2016-01-30 00:19:15 +0300

    Added Navigation drawer Added Profile Activity

---
+++ app/src/main/java/com/dita/dev/memoapp/ProfileActivity.java
@@ -0,0 +1,23 @@
+package com.dita.dev.memoapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
