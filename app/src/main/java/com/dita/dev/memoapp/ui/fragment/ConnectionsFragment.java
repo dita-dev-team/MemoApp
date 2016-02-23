97b19ca85c947d7d02800c90778108924db0e1c3
Nathan Mkaya <nathanmkaya@gmail.com>
2016-02-23 21:37:22 +0300

    Fix Connection Fragments

--- app/src/main/java/com/dita/dev/memoapp/ui/fragment/ConnectionsFragment.java
+++ app/src/main/java/com/dita/dev/memoapp/ui/fragment/ConnectionsFragment.java
@@ -2,7 +2,9 @@ package com.dita.dev.memoapp.ui.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +26,21 @@ public class ConnectionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connections, container, false);
+        View view = inflater.inflate(R.layout.fragment_connections, container, false);
+        FragmentTransaction transaction1 = getFragmentManager().beginTransaction().replace(R.id.groupContainer, new GroupFragment());
+        FragmentTransaction transaction2 = getFragmentManager().beginTransaction().replace(R.id.individualsContainer, new IndividualsFragment());
+        transaction1.commit();
+        transaction2.commit();
+
+        return view;
+    }
+
+    public static class CustomFragment extends Fragment {
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
     }
 
 }
