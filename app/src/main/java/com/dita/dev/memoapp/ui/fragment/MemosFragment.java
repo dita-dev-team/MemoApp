04486001c77406caf57c138f15782fa2b818bc0e
WHITE <mtoto.mgonjwa@gmail.com>
2016-03-18 00:44:10 +0300

    Add to Compose To Backstack Create Custom Compose Actionbar/Toolbar Remaining How to save Drafts Implementing camera and microphone Further Comments Addressing and sending of memos (How ? Will it be done)

--- app/src/main/java/com/dita/dev/memoapp/ui/fragment/MemosFragment.java
+++ app/src/main/java/com/dita/dev/memoapp/ui/fragment/MemosFragment.java
@@ -1,13 +1,24 @@
 package com.dita.dev.memoapp.ui.fragment;
 
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.dita.dev.memoapp.R;
+import com.dita.dev.memoapp.ui.activity.BaseActivity;
+import com.dita.dev.memoapp.ui.activity.ComposeActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.internal.ButterKnifeProcessor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +35,16 @@ public class MemosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memos, container, false);
+        View view = inflater.inflate(R.layout.fragment_memos, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.newMemo)
+    public void MemoClick(View view) {
+        startActivity(new Intent(getActivity(), ComposeActivity.class));
+
+
     }
 
 }
