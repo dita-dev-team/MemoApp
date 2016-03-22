cdafcf8dfb8067a8a43a1d72af4baeef0870a962
WHITE <mtoto.mgonjwa@gmail.com>
2016-03-22 22:43:04 +0300

    Merge branch 'michael_dev' of https://github.com/dita-dev-team/MemoApp into mtoto.mgonjwa
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/dita/dev/memoapp/ui/activity/ComposeActivity.java
+++ app/src/main/java/com/dita/dev/memoapp/ui/activity/ComposeActivity.java
@@ -2,20 +2,14 @@ package com.dita.dev.memoapp.ui.activity;
 
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 
 import com.dita.dev.memoapp.R;
-import com.dita.dev.memoapp.utility.ImagePickUp;
 
 public class ComposeActivity extends AppCompatActivity {
 
