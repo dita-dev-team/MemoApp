136f7149477d5b8af620e21aa3347ab3119f88ac
Arin <graffymint@gmail.com>
2016-04-05 23:51:23 +0300

    Made simple changes

--- app/src/main/java/com/dita/dev/memoapp/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/dita/dev/memoapp/ui/activity/SettingsActivity.java
@@ -13,13 +13,13 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.text.TextUtils;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 import com.dita.dev.memoapp.R;
 
@@ -141,9 +141,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         int id = item.getItemId();
         if (id == android.R.id.home) {
             if (!super.onMenuItemSelected(featureId, item)) {
-                //NavUtils.navigateUpFromSameTask(this);
-                startActivity(new Intent(this, BaseActivity.class));
-                finish();
+                NavUtils.navigateUpFromSameTask(this);
+                //startActivity(new Intent(this, BaseActivity.class));
+                //finish();
             }
             return true;
         }
